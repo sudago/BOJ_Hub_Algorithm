a544d0ba920626bb5a29b54abf492e78a97ab691
Jian <97204689+sudago@users.noreply.github.com>
2023-12-13 00:03:12 +0900

    [level 2] Title: 주차 요금 계산, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.py
@@ -9,25 +9,22 @@ def solution(fees, records):
     
     for record in records:
         time, car_num, in_out = record.split()
+        hour, minute = map(int, time.split(":")) # 분으로 바꾸는 작업
+        minutes = (hour * 60) + minute
         if in_out == "IN": # 입차라면
-            dict[car_num].append([time, 0])
+            dict[car_num].append([minutes, 0]) # 시간과 분을 합친 시간을 넣는다.
         else: # 출차라면
             for times in dict[car_num]: # 입차 기록을 찾는다
                 if times[1] == 0: # 출차 기록이 없으면 시간을 기록한다
-                    times[1] = time
+                    times[1] = minutes
                     break
                     
     for car_num, time_records in dict.items():
         cumulative_time = 0 # 누적 시간
         for time_record in time_records:
-            in_time, out_time = time_record
-            in_hour, in_minute = map(int, in_time.split(":")) # 분으로 바꾸는 작업
-            in_minutes = (in_hour * 60) + in_minute
-            if (out_time == 0):
-                out_time = "23:59"
-            out_hour, out_minute = map(int, out_time.split(":"))
-            out_minutes = (out_hour * 60) + out_minute
-            cumulative_time += out_minutes - in_minutes # 누적 시간에 계속 더한다
+            if (time_record[1] == 0):
+                time_record[1] = (23 * 60) + 59 # 23:59
+            cumulative_time += time_record[1] - time_record[0] # 동일 자동차 번호에 해당하는 (출차 시간 - 입차 시간)을 누적 시간에 계속 더한다
         cumulative_time -= default_time # 기본 시간을 빼준다
         if cumulative_time < 0: # 기본 시간을 뺀 누적 시간이 음수라면 0으로 세팅
             cumulative_time = 0
